3f0a30b70956fca688dc3d500745a141c8629bd5
Hay Kranen <huskyr@gmail.com>
2011-07-18 18:26:25 +0200

    adding html view

--- backuptweets.js
+++ backuptweets.js
@@ -1,58 +1,87 @@
-var request = require('request'),
+var fs = require('fs'),
+    request = require('request'),
     async = require('async'),
+    mustache = require('mustache'),
     _ = require('underscore');
 
 const API_COUNT_MAX = 20;
 
-module.exports = function(args, cb) {
-    var count = 0,
-        pages = Math.floor(args.max / API_COUNT_MAX),
-        tweets = [];
+var args = {
+    // Default options, will be overwritten in init with defaultArgs
+    "max" : 3200,
+    "debug" : false
+};
 
-    function getUrl(page) {
-        var url = ''.concat(
-            'http://api.twitter.com/1/statuses/user_timeline.json',
-            '?screen_name=' + args.screen_name,
-            '&page=' + page,
-            '&count=' + API_COUNT_MAX,
-            '&trim_user=' + true,
-            '&include_rts=1'
-        );
+function formatResults(results, cb) {
+    htmlFormatResults(results, function(html) {
+        cb({
+            "html" : html,
+            "json" : JSON.stringify(results),
+            "js" : results
+        });
+    })
+}
 
-        return url;
+function getUrl(page) {
+    var url = ''.concat(
+        'http://api.twitter.com/1/statuses/user_timeline.json',
+        '?screen_name=' + args.user,
+        '&page=' + page,
+        '&count=' + API_COUNT_MAX,
+        '&trim_user=' + true,
+        '&include_rts=1'
+    );
+
+    return url;
+}
+
+function htmlFormatResults(results, cb) {
+    var view = {
+        "user" : args.user,
+        "tweets" : results
+    };
+
+    fs.readFile("htmlview.html", "utf-8", function(err, data) {
+        var html = mustache.to_html(data, view);
+        cb(html);
+    });
+}
+
+function log() {
+    if (args.debug) {
+        console.log.apply(this, arguments);
     }
+}
 
-    function log() {
-        if (args.debug) {
-            console.log.apply(this, arguments);
-        }
-    }
+function init(initArgs, cb) {
+    args = _.extend(args, initArgs);
 
-    // Create a series of callbacks for the requests
-    var requests = [];
+    var requests = [],
+        pages = Math.floor(args.max / API_COUNT_MAX);
+
     for (var page = 1; page < pages + 1; page++) {
         (function(page) {
-        requests.push(function(callback) {
-            var url = getUrl(page);
+            requests.push(function(callback) {
+                var url = getUrl(page);
 
-            log("Fetching", url);
-            log("Page", page);
+                log("Fetching", url);
+                log("Page", page);
 
-            request(
-                {
-                    url : url
-                },
-                function(error, response, body) {
-                    log("Got a response");
+                request(
+                    {
+                        url : url
+                    },
+                    function(error, response, body) {
+                        log("Got a response");
 
-                    if (error) {
-                        callback(error);
-                    } else {
-                        callback(null, JSON.parse(body));
+                        if (error) {
+                            callback(error);
+                        } else {
+                            callback(null, JSON.parse(body));
+                        }
                     }
-                }
-            )
-        });
+                )
+            });
         })(page);
     }
 
@@ -62,8 +91,13 @@ module.exports = function(args, cb) {
             log("An error occured", err);
             cb(false);
         } else {
-            // Mash it all together
-            cb(_.union(results));
+            // Mash all queries together
+            results = _.union(results);
+            formatResults(results, function(finalResults) {
+                cb(finalResults);
+            });
         }
     });
 }
+
+module.exports = init;
--- example.js
+++ example.js
@@ -2,15 +2,18 @@ var backuptweets = require('./backuptweets.js'),
     fs = require('fs');
 
 backuptweets({
-    "screen_name" : "huskyr",
+    "user" : "huskyr",
     "max" : 40,
     "debug" : true
 }, function(tweets) {
     if (tweets) {
-        console.log(tweets);
-        fs.writeFile("tweets.json", JSON.stringify(tweets), function() {
-            console.log('ready');
-        })
+        fs.writeFile("tweets.json", tweets.json, function() {
+            console.log('json file ready');
+        });
+
+        fs.writeFile("tweets.html", tweets.html, function() {
+            console.log('html file ready');
+        });
     } else {
         console.log("error");
     }
